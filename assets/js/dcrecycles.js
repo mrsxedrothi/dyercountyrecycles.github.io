580b46390ea13af3ea993669b70f610107865975
pat <patthropus@gmail.com>
2017-08-12 22:03:31 -0400

    Trying this

--- assets/js/dcrecycles.js
+++ assets/js/dcrecycles.js
@@ -4,6 +4,7 @@ function videoEnd(e) {
   var videoObj = document.getElementById('front-video');
   var imageObj = document.getElementById('front-image');
   videoObj.classList.add('hidden');
-  imageObj.classList.add('video-overlay').remove('hidden');
+  imageObj.classList.remove('hidden');
+  imageObj.classList.add('video-overly');
   //alert('video is over');
 }
